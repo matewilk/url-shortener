704a933aa943d6bd0a96fa5b5ece184630f158a3
Mateusz Wilk <matewilk@gmail.com>
2024-10-04 01:01:33 +0100

    plug in database

--- apps/front/src/app/layout.tsx
+++ apps/front/src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="font-[family-name:var(--font-geist-sans)]">
+          {children}
+        </div>
       </body>
     </html>
   );
