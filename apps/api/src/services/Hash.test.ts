7dbd74c3123e30a6dba75827a898435f00816d28
Mateusz Wilk <matewilk@gmail.com>
2024-10-08 22:33:12 +0100

    add fast-check, in memory db implementation

---
+++ apps/api/src/services/Hash.test.ts
@@ -0,0 +1,25 @@
+import { describe, test, expect, beforeAll } from "vitest";
+import fc from "fast-check";
+
+import { Hash, Base62 } from "./Hash";
+
+describe("Base62", () => {
+  let hash: Hash;
+
+  beforeAll(() => {
+    hash = new Base62();
+  });
+
+  test("should endoce and decode to the same positive number", () => {
+    fc.assert(
+      fc.property(fc.integer({ min: 0 }), (num) => {
+        const encoded = hash.encode(num);
+        const decoded = hash.decode(encoded);
+
+        console.log({ num, encoded, decoded });
+
+        return num === decoded;
+      })
+    );
+  });
+});
