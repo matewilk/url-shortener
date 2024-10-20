5c06bbd1d188966a497596cba2c7c83e76dd64a0
Mateusz Wilk <matewilk@gmail.com>
2024-10-20 21:51:06 +0100

    user service int test wip

---
+++ apps/api/tests/users/UserService.int.test.ts
@@ -0,0 +1,21 @@
+import { beforeEach, afterAll } from "vitest";
+import { PrismaClient } from "@prisma/client";
+
+import { UserServiceSpec } from "./UserServiceSpec";
+import { UserService } from "../../src/users/service/UserService";
+import { DbUserRepository } from "../../src/users/repository/DbUserRepository";
+import { JwtAuthService } from "../../src/auth/service/AuthService";
+
+const prisma = new PrismaClient();
+
+beforeEach(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
+});
+
+const userRepositopr = new DbUserRepository(prisma);
+const authService = new JwtAuthService();
+UserServiceSpec.run(new UserService(userRepositopr, authService));
