7c7e26712a86c23aed7922ba76385bca3fbcc687
Mateusz Wilk <matewilk@gmail.com>
2025-04-29 16:18:18 +0100

    feat(caps): use withCapabilities in expand url page

--- apps/front/src/app/(url)/expand/[hash]/page.tsx
+++ apps/front/src/app/(url)/expand/[hash]/page.tsx
@@ -1,29 +1,38 @@
-import { client } from "@shortify/api-client/client";
 import { redirect } from "next/navigation";
 
-const apiClient = client("http://localhost:3001");
+import { withCapabilities } from "@/capabilities/withCapabilities";
+import { Capabilities } from "@/capabilities/Capabilities";
 
-type Params = Promise<{
+type Params = {
   hash: string;
-}>;
+  capabilites: Capabilities;
+};
 
-export default async function Page(props: { params: Params }) {
-  const { hash } = await props.params;
+export default withCapabilities(
+  async ({
+    params,
+    capabilities,
+  }: {
+    params: Params;
+    capabilities: Capabilities;
+  }) => {
+    const { hash } = params;
 
-  const { data, error } = await apiClient.GET(`/{shortUrl}`, {
-    params: { path: { shortUrl: hash } },
-  });
+    const { data, error } = await capabilities.apiClient.GET(`/{shortUrl}`, {
+      params: { path: { shortUrl: hash } },
+    });
 
-  if (data?.url) {
-    redirect(data?.url);
-  }
+    if (data?.url) {
+      redirect(data?.url);
+    }
 
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="text-4xl">
-        {error && <div>Error</div>}
-        {!data?.url && <div>Url not found</div>}
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="text-4xl">
+          {error && <div>Error</div>}
+          {!data?.url && <div>Url not found</div>}
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
